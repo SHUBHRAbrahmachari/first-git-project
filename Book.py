2d522174b6879f194eda57b7693eec42f6c9ec4c
ShubhraBrahmachari <shubhrab2003@gmail.com>
2026-08-25 20:06:49 +0530

    added getters for name and author

--- Book.py
+++ Book.py
@@ -3,7 +3,15 @@ class Book:
         self.__name = name
         self.__author: str = author
 
+    def get_name():
+        return self.__name
+
+    def get_author():
+        return self.__author
+
 name = input()
 author = input()
 
 book = Book(name, author)
+
+print(book.get_author(), " ", book.get_name())
